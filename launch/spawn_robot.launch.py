5574425e9ff20f819e7f4b545fade7139f403767
Ander González Tomé <ander.gonzalez@ikerlan.es>
2025-09-17 09:48:36 +0000

    robot detectando y moviendose

--- launch/spawn_robot.launch.py
+++ launch/spawn_robot.launch.py
@@ -3,22 +3,25 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, TimerAction
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
 
 
 def generate_launch_description():
     # Get the launch directory
     pkg_dir = get_package_share_directory('elea_amr')
+    tb_gazebo_dir = get_package_share_directory('elea_tb_gazebo')
     
     # Launch configuration variables specific to simulation
     x_pose = LaunchConfiguration('x_pose', default='0.0')
     y_pose = LaunchConfiguration('y_pose', default='0.0')
     z_pose = LaunchConfiguration('z_pose', default='0.57025')
     world = LaunchConfiguration('world')
+    use_rviz = LaunchConfiguration('rviz', default='false')
     
     # Declare the launch arguments
     declare_x_position_cmd = DeclareLaunchArgument(
@@ -37,6 +40,11 @@ def generate_launch_description():
         'world',
         default_value='empty.sdf',
         description='Full path to world model file to load')
+        
+    declare_rviz_cmd = DeclareLaunchArgument(
+        'rviz',
+        default_value='true',
+        description='Start RViz2 automatically with this launch file.')
 
     # Specify the actions
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
@@ -81,10 +89,35 @@ def generate_launch_description():
             '/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
             '/odom@nav_msgs/msg/Odometry@ignition.msgs.Odometry',
             '/tf@tf2_msgs/msg/TFMessage@ignition.msgs.Pose_V',
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
             '/world/empty/model/elea_robot/link/base_link/sensor/lidar/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
             '--ros-args', '-r', '/world/empty/model/elea_robot/link/base_link/sensor/lidar/scan:=/scan'
+            
         ],
         output='screen')
+
+   
+    # Static transform from lidar_link to match Gazebo frame naming
+    static_transform_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='lidar_frame_publisher',
+        arguments=['0', '0', '0', '0', '0', '0', 'lidar_link', 'elea_robot/base_link/lidar'],
+        output='screen'
+    )
+
+    # RViz
+    rviz_config_file = os.path.join(pkg_dir, 'rviz', 'elea_amr.rviz')
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file],
+        condition=IfCondition(use_rviz)
+    )
+
+    
 
     # Create the launch description and populate
     ld = LaunchDescription()
@@ -94,11 +127,14 @@ def generate_launch_description():
     ld.add_action(declare_y_position_cmd)
     ld.add_action(declare_z_position_cmd)
     ld.add_action(declare_world_cmd)
+    ld.add_action(declare_rviz_cmd)
 
     # Add any conditioned actions
     ld.add_action(start_gazebo_cmd)
     ld.add_action(robot_state_publisher_node)
     ld.add_action(spawn_entity_cmd)
     ld.add_action(bridge_cmd)
+    ld.add_action(static_transform_node)
+    ld.add_action(rviz_node)
 
     return ld
